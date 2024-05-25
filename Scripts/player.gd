7514454e0a937162a4bbc27d240d3c71e416f7ad
nimoooos <100112940+nimoooos@users.noreply.github.com>
2024-05-24 20:57:39 -1000

    health bar and isAlive added
    player can die from losing health. Health bar is visible over character's head

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
+# TODO: ask why use @export?
 @export var speed=250
+@export var health_max = 100
+@export var health_current = 100
+@export var isAlive = true
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,10 +15,36 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-# movement
-	var move_vector=Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	position += move_vector.normalized()*speed*delta
-	
-# face cursor	
-	var angle = (get_viewport().get_mouse_position()-position).angle()
-	set_rotation(angle)
+	if isAlive:
+		# movement
+		var move_vector=Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+		position += move_vector.normalized()*speed*delta
+		
+		# sprite faces cursor	
+		var angle = (get_viewport().get_mouse_position()-position).angle()
+		$Player_sprite.set_rotation(angle)
+	else:
+		kill()
+
+
+# Called upon taking damage (negative delta) or healing (positive delta)
+func change_health(delta):
+	#update health_current
+	health_current = health_current+delta
+	# prevent overheal
+	if health_current > health_max:
+		health_current = health_max
+		
+	# check if dead
+	if health_current <= 0:
+		isAlive=false
+
+	# healthbar update
+	$Health_bar.max_value=health_max
+	$Health_bar.value=health_current
+
+
+# Called when character dies
+func kill():
+	print($".", "has died.")
+	# TODO: call end screen?
