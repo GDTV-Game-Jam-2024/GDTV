d0a81f6e006ed4029b3e71a1c2f17fbf34a8e6a1
Soomin Song <d.song.149@gmail.com>
2024-05-26 07:45:01 -1000

    Basic character stuff (#1)
    * Create stone1c.jpg
    
    Source: https://www.3dxo.com/textures/510_stone_4
    
    * Create characterBlue.png
    
    https://kenney.nl/assets/sports-pack
    
    * Update project.godot
    
    Change display size to 1280x720
    
    * Basic character movement and rotation
    
    Character can now move and rotate to face the cursor
    
    * health bar and isAlive added
    
    player can die from losing health. Health bar is visible over character's head
    
    * Ground changed from texture to sprite
    
    * Player update
    
    Use CharacterBody2D instead of Node2D for character
    Refactor health logic. Split change_health() to damage() and heal()
    Health bar rounded on all corners
    
    * Imported 8 direction character sheet (placeholder)
    
    Small 8-direction Characters:
    Last update: November 18, 2023
    
    By: AxulArt
    https://axulart.itch.io/small-8-direction-characters
    https://axulart.tumblr.com/
    
    LICENCE: This asset pack can be used in both free and commercial projects. You can modify it to suit your own needs, as long as you give appropriate credit.
    
    Creative Commons License:
    This work is licensed under a Creative Commons Attribution 4.0 International License.
    
    * WASD Control Implemented
    
    WASD Control Implemented in project settings
    
    * Update player.gd
    
    Moved player movement to _physics_process
    
    * Cardinal direction conversion implemented
    
    CARDINAL_DIRECTION enum added
    angle_to_direction() for conversion
    
    * Perspective top down sprite
    
    Player can face 8 directions and maintain walking

---
+++ Scripts/player.gd
@@ -0,0 +1,112 @@
+extends CharacterBody2D
+
+@export var speed=250
+@export var health_max = 100
+@export var health_current = 100
+@export var isAlive = true
+@export var isWalking = false
+
+var move_vector
+enum CARDINAL_DIRECTION {N, NE, E, SE, S, SW, W, NW}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# initialize at center of screen
+	position = Vector2(640,360)
+	move_vector=Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# receive move input
+	move_vector=Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	
+	if isAlive:
+		# check if walking
+		if move_vector==Vector2(0,0): isWalking=false
+		else: isWalking=true
+
+		# sprite faces cursor
+		var angle = (get_viewport().get_mouse_position()-position).angle()
+		
+		# set animation
+		animate_sprite(angle, isWalking)
+
+	# kill if isAlive is set to false
+	else:
+		kill()
+
+
+# Like _process, but called every physics frame (60fps)
+func _physics_process(delta):
+	if isAlive:
+		# movement
+		position += move_vector.normalized()*speed*delta
+
+
+# Reduce health
+func damage(amount):
+	health_current = health_current-amount
+	update_health_bar()
+	# check if dead
+	if health_current <= 0:
+		isAlive=false
+
+
+# Increase health
+func heal(amount):
+	health_current = health_current+amount
+	update_health_bar()
+	# prevent overheal
+	if health_current > health_max:
+		health_current = health_max
+
+
+func update_health_bar():
+	$Health_bar.max_value=health_max
+	$Health_bar.value=health_current
+
+
+# Called when character dies
+func kill():
+	print($".", "has died.")
+	# TODO: call end screen?
+
+
+# convert radian to one of 8 cardinal directions
+func angle_to_direction(angle):
+	if abs(angle)<PI/8:
+		return CARDINAL_DIRECTION.E
+	if abs(angle)>PI*7/8:
+		return CARDINAL_DIRECTION.W
+	if angle>0:
+		if angle<PI*3/8:
+			return CARDINAL_DIRECTION.SE
+		if angle<PI*5/8:
+			return CARDINAL_DIRECTION.S
+		if angle<PI*7/8:
+			return CARDINAL_DIRECTION.SW
+	else:
+		if angle>PI*-3/8:
+			return CARDINAL_DIRECTION.NE
+		if angle>PI*-5/8:
+			return CARDINAL_DIRECTION.N
+		if angle>PI*-7/8:
+			return CARDINAL_DIRECTION.NW
+
+
+# pick which animation to show based on angle and isWalking
+func animate_sprite(angle, isWalking):
+	var direction=angle_to_direction(angle)
+	var walk_state
+	var enum_to_string_dict = {
+		0: "N", 1: "NE", 2: "E", 3: "SE",
+		4: "S", 5: "SW", 6: "W", 7: "NW"
+	}
+	
+	if isWalking: walk_state="Walk"
+	else: walk_state="Idle"
+	var animation_name = walk_state+"_"+enum_to_string_dict[direction]
+	$AnimatedSprite2D.play(animation_name)
+	
